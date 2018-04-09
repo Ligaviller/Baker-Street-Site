0698d81ec2ec041eeba6e4ca84333461d873c77b
Tapoch <zvp_97@mail.ru>
2018-04-10 00:17:49 +0300

    - Красота для 404 страницы

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,5 +1,8 @@
 <?php
 
 include ROOT.'/template/parts/header.php';
-echo "<h1>404</h1>";
+echo "<div class='err404'>";
+echo "<h1 class='error error_num'>404</h1>";
+echo "<p class='error'>Страница в разработке или ее не существует.</p>";
+echo "</div>";
 include ROOT.'/template/parts/footer.php';
